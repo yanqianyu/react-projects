622cfb1c038e579edbadac9270cf5963c775d4c7
YanQianyu <284989344@qq.com>
2021-05-22 17:34:14 +0800

    chore: day 04 finished

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
+import questions from './data';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className='container'>
+        <h3>Questions And Answers About Login</h3>
+        <div className=''>
+          {data.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}/>
+            )
+          })}
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = (props) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <div className='question'>
+      <header>
+        <h4>{props.title}</h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
+      </header>
+      {showInfo && <p>{props.info}</p>}
+    </div>
+  );
 };
 
 export default Question;
